d5101638bd202d9c28af7b86db01a2ae6823e43b
othniel2471 <othnielomaidu@gmail.com>
2023-07-05 15:04:38 +0100

    added qoutes functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Calculator from './component/calculator';
+import Quote from './component/Quotes';
 
 function App() {
   return (
     <div className="App">
       <Calculator />
+      <Quote />
     </div>
   );
 }
